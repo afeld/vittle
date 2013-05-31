bead48a6420fd7cf0c9b95baa46a77f5db5c63c4
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 06:00:36 -0400

    debounce item rollover

--- src/content.js
+++ src/content.js
@@ -1,11 +1,11 @@
 (function($){
   'use strict';
 
-  $('#menu').on('mouseenter', '[name="product"]', function(){
+  $('#menu').on('mouseenter', '[name="product"]', _.debounce(function(){
     var vittle = new Vittle(this);
     console.log(vittle.tokens());
     vittle.getImageUrl(function(url){
       console.log(url);
     });
-  });
+  }, 800));
 })(Zepto);
