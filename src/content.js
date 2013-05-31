9fde3f8a570856be331f1bed1bb4faeb8cda02e9
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 06:15:01 -0400

    hide the preview after leaving the <li>

--- src/content.js
+++ src/content.js
@@ -4,14 +4,22 @@
   var $thumb = $('<img style="position:fixed; bottom:0; left:0; max-width: 220px;"/>');
   $thumb.appendTo('body');
 
-  $('#menu').on('mouseenter', '[name="product"]', _.debounce(function(){
+  var $menu = $('#menu');
+
+  $menu.on('mouseenter', '[name="product"]', _.debounce(function(){
     var vittle = new Vittle(this);
     console.log(vittle.tokens());
+
     vittle.getImageUrl(function(url){
       console.log(url);
       if (url){
         $thumb.attr('src', url);
+        $thumb.show();
       }
     });
   }, 800));
+
+  $menu.on('mouseleave', 'li', function(){
+    $thumb.hide();
+  });
 })(Zepto);
