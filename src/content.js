ea33ef3547cd6e2515130a62be0e78aa5764fa45
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 05:36:36 -0400

    leverate Vittles from content script

--- src/content.js
+++ src/content.js
@@ -2,12 +2,7 @@
   'use strict';
 
   $('#menu').on('mouseenter', '[name="product"]', function(){
-    var $el = $(this),
-      text = $el.text();
-
-    console.log('before: "' + text + '"');
-    // remove leading/trailing whitespace and item number
-    text = text.trim().replace(/^[\d\.]*\s*/, '');
-    console.log('after: "' + text + '"');
+    var vittle = new Vittle(this);
+    console.log(vittle.tokens());
   });
 })(Zepto);
