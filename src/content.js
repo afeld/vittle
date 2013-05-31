10525ca5773357d833981048950475479c98dcaf
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 03:47:22 -0400

    print text when hovered over item

--- src/content.js
+++ src/content.js
@@ -1,4 +1,13 @@
-(function(){
+(function($){
   'use strict';
-  alert('foo');
-})();
+
+  $('#menu').on('mouseenter', '[name="product"]', function(){
+    var $el = $(this),
+      text = $el.text();
+
+    console.log('before: "' + text + '"');
+    // remove leading/trailing whitespace and item number
+    text = text.trim().replace(/^[\d\.]*\s*/, '');
+    console.log('after: "' + text + '"');
+  });
+})(Zepto);
