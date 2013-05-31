6f62f00bd75fd0b4df19de1076769bc1b7e3bb12
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 05:29:34 -0400

    implement tokens()

--- specs/vittle_spec.js
+++ specs/vittle_spec.js
@@ -1,7 +1,7 @@
 describe('Vittle', function(){
   describe('#cleanText()', function(){
-    var cleanTextFor = function(start){
-      var $el = $('<div>' + start + '</div>'),
+    var cleanTextFor = function(text){
+      var $el = $('<div>' + text + '</div>'),
         vittle = new Vittle($el);
 
       return vittle.cleanText();
@@ -13,6 +13,23 @@ describe('Vittle', function(){
 
     it("should remove surrounding whitespace", function(){
       expect(cleanTextFor('    Yummy thing ')).to.eql('Yummy thing');
+    });
+  });
+
+  describe('#tokens()', function(){
+    var tokensFor = function(text){
+      var $el = $('<div>' + text + '</div>'),
+        vittle = new Vittle($el);
+
+      return vittle.tokens();
+    };
+
+    it("should give an empty set for an empty string", function(){
+      expect(tokensFor('    ')).to.eql([]);
+    });
+
+    it("should separate any text in parens", function(){
+      expect(tokensFor('outer (inner)')).to.eql(['outer', 'inner']);
     });
   });
 });
--- src/vittle.js
+++ src/vittle.js
@@ -13,7 +13,15 @@
     },
 
     tokens: function(){
+      var text = this.cleanText(),
+        match = text.match(/[^\(\)]+/g) || [];
 
+      // remove whitespace around each token
+      for (var i = 0; i < match.length; i++){
+        match[i] = match[i].trim();
+      }
+
+      return match;
     }
   });
 })(Zepto);
