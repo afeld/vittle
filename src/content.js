1899f90b703f3825b71b1ddf97e7dd34b730d41f
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 06:08:35 -0400

    show preview in bottom left

--- src/content.js
+++ src/content.js
@@ -1,11 +1,17 @@
 (function($){
   'use strict';
 
+  var $thumb = $('<img style="position:fixed; bottom:0; left:0; max-width: 220px;"/>');
+  $thumb.appendTo('body');
+
   $('#menu').on('mouseenter', '[name="product"]', _.debounce(function(){
     var vittle = new Vittle(this);
     console.log(vittle.tokens());
     vittle.getImageUrl(function(url){
       console.log(url);
+      if (url){
+        $thumb.attr('src', url);
+      }
     });
   }, 800));
 })(Zepto);
