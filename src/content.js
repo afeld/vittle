5375ecfa3f1790b833cf4b35e82691cbc0cd231d
Aidan Feldman <aidan.feldman@gmail.com>
2013-05-31 05:52:33 -0400

    fetch image URL on rollover

--- src/content.js
+++ src/content.js
@@ -4,5 +4,8 @@
   $('#menu').on('mouseenter', '[name="product"]', function(){
     var vittle = new Vittle(this);
     console.log(vittle.tokens());
+    vittle.getImageUrl(function(url){
+      console.log(url);
+    });
   });
 })(Zepto);
--- src/vittle.js
+++ src/vittle.js
@@ -22,6 +22,26 @@
       }
 
       return match;
+    },
+
+    getImageUrl: function(callback){
+      var tokens = this.tokens(),
+        term = tokens[tokens.length - 1];
+
+      if (term){
+        var searchUrl = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f84e509282558d46d32f367b0d7f501&text=' + encodeURIComponent(term) + '&sort=relevance&format=json&nojsoncallback=1&per_page=1&media=photos';
+        $.getJSON(searchUrl, function(data){
+          var photo = data.photos.photo[0];
+          if (photo){
+            var imageUrl = 'http://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+            callback(imageUrl);
+          } else {
+            callback(null);
+          }
+        });
+      } else {
+        callback(null);
+      }
     }
   });
 })(Zepto);
