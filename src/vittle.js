cac3d177b63bfba1d9adb383a607222d65ebb3ef
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-15 14:26:49 -0400

    update API key

--- src/vittle.js
+++ src/vittle.js
@@ -34,7 +34,7 @@
         term = tokens[tokens.length - 1];
 
       if (term){
-        var searchUrl = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f84e509282558d46d32f367b0d7f501&text=' + encodeURIComponent(term) + '&sort=relevance&format=json&nojsoncallback=1&per_page=1&media=photos&extras=' + FLICKR_EXTRAS;
+        var searchUrl = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e85aff8ce1c2fc44b92bccff30f92f7d&text=' + encodeURIComponent(term) + '&sort=relevance&format=json&nojsoncallback=1&per_page=1&media=photos&extras=' + FLICKR_EXTRAS;
         $.getJSON(searchUrl, function(data){
           var photo = data.photos.photo[0];
           if (photo){
