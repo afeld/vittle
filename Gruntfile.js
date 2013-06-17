3a9adf8e2a737bde74e9cf2f78f7c501d6a33491
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-17 01:21:27 -0400

    add grunt compress task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,20 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    compress: {
+      main: {
+        options: {
+          archive: '../vittle.zip'
+        },
+        src: ['**/*', '!.git/', '!specs/', '!node_modules/', '*sublime-*']
+      }
+    },
     mocha: {
       all: ['specs/index.html']
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-mocha');
 
   // Default task(s).
